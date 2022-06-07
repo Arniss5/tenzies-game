311483dad8383b5625b29894735889051d59f029
Arniss5 <kat.dobr@gmail.com>
2022-06-07 23:03:16 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <h2>Author: Kasia</h2>
+
+    </div>
+  );
+}
+
+export default App;
