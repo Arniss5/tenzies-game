7a8b8d03ecf8be13fa4c79b93ba1d956412eb046
Arniss5 <kat.dobr@gmail.com>
2022-06-10 08:41:25 +0100

    add Die component

---
+++ src/Die.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Die(props) {
+
+
+    return (
+        <div 
+            className={props.isHeld ? "die held" : "die"}
+            onClick={props.holdDice}
+        >
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
